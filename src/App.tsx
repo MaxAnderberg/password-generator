1b10cb732de0d85fecb8c3fff2f98289e0ecceaf
Max <maxanderberg@gmail.com>
2023-07-12 21:18:47 +0200

    set up basic input of type range

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,29 @@
-import './App.css'
+import { useState } from "react";
+import "./App.css";
 
 function App() {
-
+  const [value, setValue] = useState('20')
+  const handleCharLength = (event) => {
+    setValue(event.target.value)
+  }
   return (
-    <div className="App flex flex-col items-center mt-[133px] text-[#E6E5EA]">
-      <h1 className='text-[#817D92]'>Password Generator</h1>
-      <section>
-        input here
+    <div className="App flex flex-col items-center mt-[64px] text-[#E6E5EA] mx-[16px] overflow-hidden">
+      <h1 className="text-[#817D92] mb-[16px]">Password Generator</h1>
+      <section className="bg-[#24232C] w-full mb-[16px] p-[16px]">
+        Generated Password
       </section>
-      <article>
-        <section>
-          body of the thingie here
+      <article className="bg-[#24232C] p-[16px] w-full">
+        <section className="flex justify-between">
+          <span>Character Length</span>
+          <span>{value}</span>
         </section>
+        <section className=''>
+          <input type="range" onChange={handleCharLength} className="w-full" min='0' max='20'/>
+        </section>
+        <button className="bg-[#A4FFAF] text-[#24232C] w-full">Generate</button>
       </article>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
