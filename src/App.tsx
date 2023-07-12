40a4881ea13c1afa955ac98407c3c288ffdee768
Max <maxanderberg@gmail.com>
2023-07-12 11:36:13 +0200

    basic html layout

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,15 @@ function App() {
 
   return (
     <div className="App">
-      <h1>Hello</h1> 
+      <h1>Password Generator</h1>
+      <section>
+        input here
+      </section>
+      <article>
+        <section>
+          body of the thingie here
+        </section>
+      </article>
     </div>
   )
 }
