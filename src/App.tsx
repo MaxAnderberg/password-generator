d1a2b47363c845ecc3e62e2c8648cd041ed4498a
Max <maxanderberg@gmail.com>
2023-07-12 11:45:15 +0200

    added styling

--- src/App.tsx
+++ src/App.tsx
@@ -3,8 +3,8 @@ import './App.css'
 function App() {
 
   return (
-    <div className="App">
-      <h1>Password Generator</h1>
+    <div className="App flex flex-col items-center mt-[133px] text-[#E6E5EA]">
+      <h1 className='text-[#817D92]'>Password Generator</h1>
       <section>
         input here
       </section>
