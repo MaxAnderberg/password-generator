97f85fae69c5269b3a8d12640faa2807be17be6f
Max <maxanderberg@gmail.com>
2023-07-14 22:34:50 +0200

    refactored and exported PasswordConfig interface

--- src/App.tsx
+++ src/App.tsx
@@ -3,15 +3,15 @@ import "./App.css";
 import DisplayGeneratedPassword from "./components/DisplayGeneratedPassword";
 import PasswordGenerator from "./components/PasswordGenerator";
 
-function App() {
-  interface PasswordConfig {
-    uppercase: boolean
-    lowercase: boolean
-    numbers: boolean
-    symbols: boolean
-  }
+export interface PasswordConfig {
+  uppercase: boolean
+  lowercase: boolean
+  numbers: boolean
+  symbols: boolean
+}
 
-  const [value, setValue] = useState("20");
+function App() {
+  
   const [passwordConfig, setPasswordConfig] = useState<PasswordConfig>({
     lowercase: false,
     uppercase: false,
@@ -19,15 +19,11 @@ function App() {
     symbols: false,
   })
 
-  const handleCharLength = (event: ChangeEvent<HTMLInputElement>) => {
-    setValue(event.target.value);
-  };
-
   return (
     <div className="App flex flex-col items-center mt-[64px] text-[#E6E5EA] mx-[16px] overflow-hidden font-[JetBrains-Mono] font-bold">
       <h1 className="text-[#817D92] mb-[16px]">Password </h1>
-      <DisplayGeneratedPassword />
-      <PasswordGenerator handleCharLength={handleCharLength} value={value} />
+      <DisplayGeneratedPassword passwordConfig={passwordConfig}/>
+      <PasswordGenerator setPasswordConfig={setPasswordConfig}/>
     </div>
   );
 }
