e0e1656de5a0deb6cac19bc525a686603611c5d3
Max <maxanderberg@gmail.com>
2023-07-14 17:12:19 +0200

    refactored checkboxes

--- src/components/PasswordGenerator.tsx
+++ src/components/PasswordGenerator.tsx
@@ -1,11 +1,30 @@
+import { useState, ChangeEvent, FormEvent } from 'react'
+import CheckBox from './CheckBox';
+
 type Props = {
 	handleCharLength: Function;
 	value: string;
 };
 
+/*
+	TODO: Create an on submit handler
+	TODO: Create an verify form is working
+	TODO: Hook form up to global state
+*/
+
 const PasswordGenerator = ({ handleCharLength, value}: Props) => {
+
+	const [uppercase, setUppercase] = useState(false)
+	const [lowercase, setLowercase] = useState(false)
+	const [numbers, setNumbers] = useState(false)
+	const [symbols, setSymbols] = useState(false)
+	
+	const handleOnSubmit = ( event:FormEvent<HTMLFormElement> ) => {
+		event.preventDefault()
+	}
+
 	return (
-		<form className="bg-dark-grey p-[16px] w-full">
+		<form className="bg-dark-grey p-[16px] w-full" onSubmit={handleOnSubmit}>
 			<section className="flex justify-between mb-[23px]">
 				<span className=''>Character Length</span>
 				<span className='text-[#A4FFAF] text-[24px] font-bold'>{value}</span>
@@ -20,22 +39,10 @@ const PasswordGenerator = ({ handleCharLength, value}: Props) => {
 				/>
 			</section>
 			<section className="flex flex-col items-start gap-[17px] mb-[32px]">
-				<section className="flex flex-row-reverse">
-					<label>Include Uppercase Letters</label>
-					<input type="checkbox" className="mr-[20px] h-[20px] w-[20px] bg-transparent border-[#E6E5EA] checked:bg-neon-green " />
-				</section>
-				<section className="flex flex-row-reverse">
-					<label>Include Lowercase Letters</label>
-					<input type="checkbox" className="mr-[20px] h-[20px] w-[20px] bg-transparent border-[#E6E5EA] checked:bg-neon-green" />
-				</section>
-				<section className="flex flex-row-reverse">
-					<label>Include Numbers</label>
-					<input type="checkbox" className="mr-[20px] h-[20px] w-[20px] bg-transparent border-[#E6E5EA] checked:bg-neon-green" />
-				</section>
-				<section className="flex flex-row-reverse">
-					<label>Include Symbols</label>
-					<input type="checkbox" className="mr-[20px] h-[20px] w-[20px] bg-transparent border-[#E6E5EA] checked:bg-neon-green" />
-				</section>
+				<CheckBox label='Include Uppercase Letters' checked={uppercase} setChecked={setUppercase}/>
+				<CheckBox label='Include Lowercase Letters' checked={lowercase} setChecked={setLowercase}/>
+				<CheckBox label='Include Numbers' checked={numbers} setChecked={setNumbers}/>
+				<CheckBox label='Include Symbols' checked={symbols} setChecked={setSymbols}/>
 			</section>
 			<section className='flex bg-[#18171F] p-[17px] mb-[16px] justify-between'>
 				<p className='text-[#817D92]'>STRENGTH</p>
