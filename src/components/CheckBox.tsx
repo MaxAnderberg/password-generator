f86fe7c8a6c5f5c42b2ae511550f7bbca9536a67
Max <maxanderberg@gmail.com>
2023-07-14 17:12:02 +0200

    init checkbox component

---
+++ src/components/CheckBox.tsx
@@ -0,0 +1,18 @@
+import React from 'react'
+
+type Props = {
+    label: string
+    checked: boolean
+    setChecked: React.Dispatch<React.SetStateAction<boolean>>
+}
+
+const CheckBox = ({label, checked, setChecked}:Props) => {
+  return (
+    <section className="flex flex-row-reverse">
+        <label>{label}</label>
+        <input type="checkbox" className="mr-[20px] h-[20px] w-[20px] bg-transparent border-[#E6E5EA] checked:bg-neon-green " onChange={() => setChecked(!checked)}/>
+    </section>
+  )
+}
+
+export default CheckBox
