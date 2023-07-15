1ce0562969df6663df017a26aef98dfaecb8cc53
Max Anderberg <maxanderberg@gmail.com>
2023-07-15 21:36:01 +0200

    added basic password generation

--- src/components/DisplayGeneratedPassword.tsx
+++ src/components/DisplayGeneratedPassword.tsx
@@ -5,8 +5,29 @@ interface Props {
 	passwordConfig: PasswordConfig
 }
 
+const generatePassword = (config: PasswordConfig): string => {
+  const upperChars = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+  const lowerChars = 'abcdefghijklmnopqrstuvwxyz';
+  const numberChars = '0123456789';
+  const symbolChars = '!@#$%^&*()-=_+[]{}|;:,.<>?';
+
+  let allCharacters = '';
+  if (config.uppercase) allCharacters += upperChars;
+  if (config.lowercase) allCharacters += lowerChars;
+  if (config.numbers) allCharacters += numberChars;
+  if (config.symbols) allCharacters += symbolChars;
+
+  let password = ''
+
+  for(let i = 1; i <= config.charLength; i++) {
+  	const randomIndex = Math.floor(Math.random() * allCharacters.length)
+  	password += allCharacters[randomIndex]
+  }
+
+  return password;
+}
+
 const DisplayGeneratedPassword = ({passwordConfig}: Props) => {
-	console.log(passwordConfig)
 	return (
 	      <section className="bg-dark-grey w-full mb-[16px] p-[16px] flex">
 	        <p className='text-almost-white text-[22px]'>
