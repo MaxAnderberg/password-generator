982a31d4a3057fbf1a8fef7285ade4f79167c085
Max Anderberg <maxanderberg@gmail.com>
2023-07-15 16:37:21 +0200

    formatted

--- src/components/CheckBox.tsx
+++ src/components/CheckBox.tsx
@@ -1,16 +1,16 @@
 import React from 'react'
 
 type Props = {
-    label: string
-    checked: boolean
-    setChecked: React.Dispatch<React.SetStateAction<boolean>>
+  label: string
+  checked: boolean
+  setChecked: React.Dispatch<React.SetStateAction<boolean>>
 }
 
-const CheckBox = ({label, checked, setChecked}:Props) => {
+const CheckBox = ({ label, checked, setChecked }: Props) => {
   return (
     <section className="flex flex-row-reverse">
-        <label>{label}</label>
-        <input type="checkbox" className="mr-[20px] h-[20px] w-[20px] bg-transparent border-[#E6E5EA] checked:bg-neon-green " onChange={() => setChecked(!checked)}/>
+      <label>{label}</label>
+      <input type="checkbox" className="mr-[20px] h-[20px] w-[20px] bg-transparent border-[#E6E5EA] checked:bg-neon-green " onChange={() => setChecked(!checked)} />
     </section>
   )
 }
