6907a95a4941310ffd70c06511a027dcf5f9e0b2
Max Anderberg <maxanderberg@gmail.com>
2023-07-17 13:58:03 +0200

    added rough foundation for displaying correct password strength

--- src/components/DisplayPasswordStrength.tsx
+++ src/components/DisplayPasswordStrength.tsx
@@ -1,13 +1,66 @@
-export const DisplayPasswordStrength = () => {
+type Props = {
+	passwordStrength: number
+}
+
+export const DisplayPasswordStrength = ({ passwordStrength }: Props) => {
 	return (
 		<section className='flex bg-[#18171F] p-[17px] mb-[16px] justify-between'>
 			<p className='text-[#817D92]'>STRENGTH</p>
 			<p className='text-[#817D92]'>TOO WEAK</p>
 			<div className='flex gap-[8px]'>
-				<span className='border-white w-[10px] h-[20px] bg-red-500'> </span>
-				<span className='border-white w-[10px] h-[20px] bg-red-500'> </span>
-				<span className='border-white w-[10px] h-[20px] bg-red-500'> </span>
-				<span className='border-white w-[10px] h-[20px] bg-red-500'> </span>
+				{
+					passwordStrength === 1
+					? (
+						<>
+						<span className='border-white w-[10px] h-[20px] bg-red-500'> </span> 
+						<span className='border-[#E6E5EA] border-[2px] w-[10px] h-[20px] bg-transparent'> </span> 
+						<span className='border-[#E6E5EA] border-[2px] w-[10px] h-[20px] bg-transparent'> </span> 
+						<span className='border-[#E6E5EA] border-[2px] w-[10px] h-[20px] bg-transparent'> </span> 
+						</>
+						)
+					: ''
+				}
+
+				{
+					passwordStrength === 2
+					? (
+						<>
+						<span className='border-white w-[10px] h-[20px] bg-[#FB7C58]'> </span> 
+						<span className='border-white w-[10px] h-[20px] bg-[#FB7C58]'> </span>
+						<span className='border-[#E6E5EA] border-[2px] w-[10px] h-[20px] bg-transparent'> </span>
+						<span className='border-[#E6E5EA] border-[2px] w-[10px] h-[20px] bg-transparent'> </span>
+						</>
+						)
+					: ''
+				}
+
+								{
+					passwordStrength === 3
+					? (
+						<>
+						<span className='border-white w-[10px] h-[20px] bg-[#F8CD65]'> </span> 
+						<span className='border-white w-[10px] h-[20px] bg-[#F8CD65]'> </span>
+						<span className='border-white w-[10px] h-[20px] bg-[#F8CD65]'> </span>
+						<span className='border-[#E6E5EA] border-[2px] h-[20px] bg-transparent'> </span>
+						</>
+						)
+					: ''
+				}
+
+								{
+					passwordStrength === 4
+					? (
+						<>
+						<span className='border-white w-[10px] h-[20px] bg-[#A4FFAF]'> </span> 
+						<span className='border-white w-[10px] h-[20px] bg-[#A4FFAF]'> </span>
+						<span className='border-white w-[10px] h-[20px] bg-[#A4FFAF]'> </span>
+						<span className='border-white w-[10px] h-[20px] bg-[#A4FFAF]'> </span>
+						</>
+						)
+					: ''
+				}
+
+
 			</div>
 		</section>
 	)
