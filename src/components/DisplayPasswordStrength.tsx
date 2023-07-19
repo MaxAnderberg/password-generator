82240ce0334b6db11769918335ca208f632789cb
Max Anderberg <maxanderberg@gmail.com>
2023-07-19 09:52:14 +0200

    updated password strength to include text indication

--- src/components/DisplayPasswordStrength.tsx
+++ src/components/DisplayPasswordStrength.tsx
@@ -6,13 +6,13 @@ type Props = {
 export const DisplayPasswordStrength = ({ passwordStrength }: Props) => {
 	return (
 		<section className='flex bg-[#18171F] p-[17px] mb-[16px] justify-between'>
-			<p className='text-[#817D92]'>STRENGTH</p>
-			<p className='text-[#817D92]'>TOO WEAK</p>
+			<p className='text-[#817D92] text-[16px]'>STRENGTH</p>
 			<div className='flex gap-[8px]'>
 				{
 					passwordStrength === 1
 						? (
 							<>
+								<p className='text-[#E6E5EA] text-[18px]'>TOO WEAK</p>
 								<span className='w-[10px] h-[20px] bg-red-500'> </span> 
 								<span className='border-[#E6E5EA] border-[2px] w-[10px] h-[20px] bg-transparent'> </span> 
 								<span className='border-[#E6E5EA] border-[2px] w-[10px] h-[20px] bg-transparent'> </span> 
@@ -26,6 +26,7 @@ export const DisplayPasswordStrength = ({ passwordStrength }: Props) => {
 					passwordStrength === 2
 						? (
 							<>
+								<p className='text-[#E6E5EA] text-[18px]'>WEAK</p>
 								<span className='w-[10px] h-[20px] bg-[#FB7C58]'> </span> 
 								<span className='w-[10px] h-[20px] bg-[#FB7C58]'> </span>
 								<span className='border-[#E6E5EA] border-[2px] w-[10px] h-[20px] bg-transparent'> </span>
@@ -39,6 +40,7 @@ export const DisplayPasswordStrength = ({ passwordStrength }: Props) => {
 					passwordStrength === 3
 						? (
 							<>
+								<p className='text-[#E6E5EA] text-[18x]'>MEDIUM</p>
 								<span className='w-[10px] h-[20px] bg-[#F8CD65]'> </span> 
 								<span className='w-[10px] h-[20px] bg-[#F8CD65]'> </span>
 								<span className='w-[10px] h-[20px] bg-[#F8CD65]'> </span>
@@ -52,6 +54,7 @@ export const DisplayPasswordStrength = ({ passwordStrength }: Props) => {
 					passwordStrength === 4
 						? (
 							<>
+								<p className='text-[#E6E5EA] text-[18px]'>STRONG</p>
 								<span className='w-[10px] h-[20px] bg-[#A4FFAF]'> </span> 
 								<span className='w-[10px] h-[20px] bg-[#A4FFAF]'> </span>
 								<span className='w-[10px] h-[20px] bg-[#A4FFAF]'> </span>
