58c95819f0ca1a9e60287f53137e4a4d8467898c
Max Anderberg <maxanderberg@gmail.com>
2023-07-21 11:06:30 +0200

    fixed margin and height of the password bars

--- src/components/DisplayPasswordStrength.tsx
+++ src/components/DisplayPasswordStrength.tsx
@@ -5,18 +5,18 @@ type Props = {
 
 export const DisplayPasswordStrength = ({ passwordStrength }: Props) => {
 	return (
-		<section className='flex bg-[#18171F] px-[16px] py-[17px] mb-[16px] md:mb-[32px] md:px-[32px] md:py-[23px] items-center justify-between'>
-			<p className='text-[#817D92] text-[16px]'>STRENGTH</p>
-			<div className='flex gap-[8px]'>
+		<section className='flex bg-[#18171F] px-[16px] py-[17px] mb-[16px] md:mb-[32px] md:px-[32px] md:py-[14px] items-center justify-between'>
+			<p className='text-[#817D92] text-[16px] md:text-[18px]'>STRENGTH</p>
+			<div className='flex gap-[8px] items-center'>
 				{
 					passwordStrength === 1
 						? (
 							<>
-								<p className='text-[#E6E5EA] text-[18px]'>TOO WEAK</p>
-								<span className='w-[10px] h-[20px] bg-red-500'> </span> 
-								<span className='border-[#E6E5EA] border-[2px] w-[10px] h-[20px] bg-transparent'> </span> 
-								<span className='border-[#E6E5EA] border-[2px] w-[10px] h-[20px] bg-transparent'> </span> 
-								<span className='border-[#E6E5EA] border-[2px] w-[10px] h-[20px] bg-transparent'> </span> 
+								<p className='text-[#E6E5EA] text-[18px] md:text-[24px] mr-[8px]'>TOO WEAK</p>
+								<span className='w-[10px] h-[28px] bg-red-500'> </span> 
+								<span className='border-[#E6E5EA] border-[2px] w-[10px] h-[28px] bg-transparent'> </span> 
+								<span className='border-[#E6E5EA] border-[2px] w-[10px] h-[28px] bg-transparent'> </span> 
+								<span className='border-[#E6E5EA] border-[2px] w-[10px] h-[28px] bg-transparent'> </span> 
 							</>
 						)
 						: ''
@@ -26,11 +26,11 @@ export const DisplayPasswordStrength = ({ passwordStrength }: Props) => {
 					passwordStrength === 2
 						? (
 							<>
-								<p className='text-[#E6E5EA] text-[18px]'>WEAK</p>
-								<span className='w-[10px] h-[20px] bg-[#FB7C58]'> </span> 
-								<span className='w-[10px] h-[20px] bg-[#FB7C58]'> </span>
-								<span className='border-[#E6E5EA] border-[2px] w-[10px] h-[20px] bg-transparent'> </span>
-								<span className='border-[#E6E5EA] border-[2px] w-[10px] h-[20px] bg-transparent'> </span>
+								<p className='text-[#E6E5EA] text-[18px] md:text-[24px] mr-[8px]'>WEAK</p>
+								<span className='w-[10px] h-[28px] bg-[#FB7C58]'> </span> 
+								<span className='w-[10px] h-[28px] bg-[#FB7C58]'> </span>
+								<span className='border-[#E6E5EA] border-[2px] w-[10px] h-[28px] bg-transparent'> </span>
+								<span className='border-[#E6E5EA] border-[2px] w-[10px] h-[28px] bg-transparent'> </span>
 							</>
 						)
 						: ''
@@ -40,11 +40,11 @@ export const DisplayPasswordStrength = ({ passwordStrength }: Props) => {
 					passwordStrength === 3
 						? (
 							<>
-								<p className='text-[#E6E5EA] text-[18x]'>MEDIUM</p>
-								<span className='w-[10px] h-[20px] bg-[#F8CD65]'> </span> 
-								<span className='w-[10px] h-[20px] bg-[#F8CD65]'> </span>
-								<span className='w-[10px] h-[20px] bg-[#F8CD65]'> </span>
-								<span className='border-[#E6E5EA] border-[2px] w-[10px] h-[20px] bg-transparent'> </span>
+								<p className='text-[#E6E5EA] text-[18x] md:text-[24px] mr-[8px]'>MEDIUM</p>
+								<span className='w-[10px] h-[28px] bg-[#F8CD65]'> </span> 
+								<span className='w-[10px] h-[28px] bg-[#F8CD65]'> </span>
+								<span className='w-[10px] h-[28px] bg-[#F8CD65]'> </span>
+								<span className='border-[#E6E5EA] border-[2px] w-[10px] h-[28px] bg-transparent'> </span>
 							</>
 						)
 						: ''
@@ -54,7 +54,7 @@ export const DisplayPasswordStrength = ({ passwordStrength }: Props) => {
 					passwordStrength === 4
 						? (
 							<>
-								<p className='text-[#E6E5EA] text-[18px]'>STRONG</p>
+								<p className='text-[#E6E5EA] text-[18px] md:text-[24px] mr-[8px]'>STRONG</p>
 								<span className='w-[10px] h-[20px] bg-[#A4FFAF]'> </span> 
 								<span className='w-[10px] h-[20px] bg-[#A4FFAF]'> </span>
 								<span className='w-[10px] h-[20px] bg-[#A4FFAF]'> </span>
