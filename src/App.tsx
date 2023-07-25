cef4d8a2715c4ce24d4e846d43c48a1d5848a0a5
Max Anderberg <maxanderberg@gmail.com>
2023-07-25 12:27:03 +0200

    fixed title

--- src/App.tsx
+++ src/App.tsx
@@ -30,7 +30,7 @@ function App() {
   return (
 
     <div className="App flex flex-col items-center mt-[64px] md:mt-[133px] text-[#E6E5EA] mx-[16px] md:m-auto md:max-w-[540px] overflow-hidden">
-      <h1 className="text-[#817D92] mb-[16px]">Password</h1>
+      <h1 className="text-[#817D92] mb-[16px] md:text-[24px] md:mb-[31px]">Password Generator</h1>
       <DisplayGeneratedPassword password={password}/>
       <PasswordForm handleGeneratePassword={handleGeneratePassword} passwordStrength={passwordStrength}/>
     </div>
