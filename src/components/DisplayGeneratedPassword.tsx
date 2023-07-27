6aba73d95f351c4971f9b94bc05d3436f9c98093
Max Anderberg <maxanderberg@gmail.com>
2023-07-27 09:31:22 +0200

    formatting

--- src/components/DisplayGeneratedPassword.tsx
+++ src/components/DisplayGeneratedPassword.tsx
@@ -2,14 +2,14 @@ import { useCallback, useState } from "react";
 import CopyIcon from "./CopyIcon";
 
 interface Props {
-  password: string
+	password: string
 }
 
 const DisplayGeneratedPassword = ({ password }: Props) => {
 
-  const copyPassword = useCallback(() => {
-    navigator.clipboard.writeText(password);
-  }, [password]);
+	const copyPassword = useCallback(() => {
+		navigator.clipboard.writeText(password);
+	}, [password]);
 
 	const [hasCopied, setCopied] = useState(false)
 
@@ -17,21 +17,21 @@ const DisplayGeneratedPassword = ({ password }: Props) => {
 		setCopied(true)
 		setTimeout(() => {
 			setCopied(false)
-		}	, 10000)
+		}, 10000)
 	}
 
 	return (
-	      <section className="bg-dark-grey w-full mb-[16px] p-[16px] md:px-[32px] py-[19px] flex items-center" onClick={copyPassword}>
-	        <p className={` cursor-default text-almost-white ${password ? 'opacity-100' : 'opacity-25'}  text-[18px] md:text-[24px]`}>
-	          {password ? password : 'P4$5W0rD!'}
-	        </p>
-	        <section className='cursor-pointer ml-auto flex text-center' onClick={copyPassword}>
-					<p className={`text-[#A4FFAF] uppercase mr-[10px] md:mr-[16px] text-center text-[14px] md:text-[18px] ${hasCopied ? 'visible' : 'invisible'}`}>Copied</p>
-					<section onClick={handleCopyingPassword}>
-						<CopyIcon />	        	
-					</section>
-	        </section>
-	      </section>
+		<section className="bg-dark-grey w-full mb-[16px] p-[16px] md:px-[32px] py-[19px] flex items-center" onClick={copyPassword}>
+			<p className={` cursor-default text-almost-white ${password ? 'opacity-100' : 'opacity-25'}  text-[18px] md:text-[24px]`}>
+				{password ? password : 'P4$5W0rD!'}
+			</p>
+			<section className='cursor-pointer ml-auto flex text-center' onClick={copyPassword}>
+				<p className={`text-[#A4FFAF] uppercase mr-[10px] md:mr-[16px] text-center text-[14px] md:text-[18px] ${hasCopied ? 'visible' : 'invisible'}`}>Copied</p>
+				<section onClick={handleCopyingPassword}>
+					<CopyIcon />
+				</section>
+			</section>
+		</section>
 	);
 };
 
